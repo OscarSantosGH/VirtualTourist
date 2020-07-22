abcbf1114389ebf07d9a0f64311a9a6da741c7a4
Oscar Santos <oscarsantos.go@gmail.com>
2020-07-21 23:22:35 -0400

    added: TravelLocationsMapViewController class for the map view

---
+++ VirtualTourist/Controllers/TravelLocationsMapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TravelLocationsMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Oscar Santos on 7/21/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TravelLocationsMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
