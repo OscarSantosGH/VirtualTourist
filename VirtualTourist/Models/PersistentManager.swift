e28ee1c6768496caee8b6f849c76d9838ba60171
Oscar Santos <oscarsantos.go@gmail.com>
2020-07-30 18:51:36 -0400

    added: PersistentManager class to  create the CoreData stack

---
+++ VirtualTourist/Models/PersistentManager.swift
@@ -0,0 +1,27 @@
+//
+//  PersistentManager.swift
+//  VirtualTourist
+//
+//  Created by Oscar Santos on 7/30/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PersistentManager{
+    let persistentContainer:NSPersistentContainer
+    
+    init(modelName:String){
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil){
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else{
+                fatalError(error!.localizedDescription)
+            }
+            completion?()
+        }
+    }
+}
